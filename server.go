dcb23c23e342b68c6b829dbc4752c37c7f028c01
Jose Naylor <josenaylor@Joses-Macbook-Air-M2.local>
2024-03-11 00:12:08 -0400

    Added routes for users along with basic handler functions

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -12,5 +13,37 @@ func main() {
 		return c.String(http.StatusOK, "Hello World")
 	})
 
+	//user routes
+	// create
+	e.POST("/users", saveUser)
+	// read
+	e.GET("/user/:id", getUser)
+	// update
+	e.PATCH("/user/:id", updateUser)
+	// delete
+	e.DELETE("user/:id", deleteUser)
+
+	//todo routes
+
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func saveUser(c echo.Context) error {
+	user := "You created a new user!"
+	return c.String(http.StatusOK, user)
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, fmt.Sprintf("You are user: %s", id))
+}
+
+func updateUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, fmt.Sprintf("You are updated: %s", id))
+}
+
+func deleteUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, fmt.Sprintf("You deleted: %s", id))
 }
