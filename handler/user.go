911a46bb61a079c595c7e0585564ad0aa1d7ac4f
Jose Naylor <josenaylor@Joses-Macbook-Air-M2.local>
2024-03-12 14:27:30 -0400

    moved handlers to seperate package

---
+++ handler/user.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (h *Handler) SaveUser(c echo.Context) error {
+	user := "You created a new user!"
+	return c.String(http.StatusOK, user)
+}
+
+func (h *Handler) GetUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, fmt.Sprintf("You are user: %s", id))
+}
+
+func (h *Handler) UpdateUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, fmt.Sprintf("You are updated: %s", id))
+}
+
+func (h *Handler) DeleteUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, fmt.Sprintf("You deleted: %s", id))
+}
--- server.go
+++ server.go
@@ -1,49 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/Jose-N/go-htmx-todo/handler"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+
+	h := &handler.Handler{}
+
+	// Index
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World")
 	})
 
 	//user routes
-	// create
-	e.POST("/users", saveUser)
-	// read
-	e.GET("/user/:id", getUser)
-	// update
-	e.PATCH("/user/:id", updateUser)
-	// delete
-	e.DELETE("user/:id", deleteUser)
+	e.POST("/users", h.SaveUser)
+	e.GET("/user/:id", h.GetUser)
+	e.PATCH("/user/:id", h.UpdateUser)
+	e.DELETE("user/:id", h.DeleteUser)
 
 	//todo routes
 
 	e.Logger.Fatal(e.Start(":8080"))
-}
-
-func saveUser(c echo.Context) error {
-	user := "You created a new user!"
-	return c.String(http.StatusOK, user)
-}
-
-func getUser(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusOK, fmt.Sprintf("You are user: %s", id))
-}
-
-func updateUser(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusOK, fmt.Sprintf("You are updated: %s", id))
-}
-
-func deleteUser(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusOK, fmt.Sprintf("You deleted: %s", id))
 }
